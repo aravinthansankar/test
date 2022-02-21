2d00ae5f919996aed833ea6305358cd9fff1c961
aravinthan <aravinthansankar97@gmail.com>
2022-02-21 12:15:34 +1100

    fix video

--- src/screen/Info/InfoScreen.js
+++ src/screen/Info/InfoScreen.js
@@ -2,12 +2,16 @@ import { View, Text,Linking} from 'react-native'
 import { useRoute } from '@react-navigation/native';
 import { WebView } from 'react-native-webview';
 import React,{useState, useEffect} from 'react'
+import { Video, AVPlaybackStatus } from 'expo-av';
 
 
 
 const  InfoScreen = () => {
   const route = useRoute()
- 
+  const video = React.useRef(null);
+  const [status, setStatus] = React.useState({});
+
+
 
   return (
     <View style={{flex:1}}>
@@ -19,15 +23,42 @@ const  InfoScreen = () => {
       </View>
 
       <View style={{flex:1, backgroundColor:'red'}}>
-      <WebView
-   source={{html: `<iframe width="100%" height="50%" src=${route.params.Embed_url} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>`}}
+
+     
+      {/* <WebView
+   source={{html: `<iframe width="100%" height="50%" originWhitelist=${route.params.Embed_url} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>`}}
    style={{marginTop: 20, backgroundColor:'green'}}
+/> */}
+<WebView
+   source={{ uri: route.params.Embed_url }}
+   originWhitelist={['https://*']}
+   startInLoadingState={true}
+  renderLoading={() => <Text>Lodding</Text>}
 />
       </View>
  
     </View>
   )
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+  },
+  video: {
+    alignSelf: 'center',
+    width: 320,
+    height: 200,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 export default InfoScreen
 
 
